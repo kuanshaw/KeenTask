21072f7c8296121d85d74c88c7bc6716345020a4
kuanshaw <1300210573@qq.com>
2021-04-10 00:25:58 +0800

    增加注释

--- k_rtos.c
+++ k_rtos.c
@@ -1,10 +1,18 @@
 #include "k_rtos.h"
 
-
-
+/** 
+ * 滴答计数值   
+ */ 
 static uint16_t _tick = 0;
+
+/** 
+ * 最大任务执行周期
+ */ 
 static uint16_t _period_max = 0;
 
+/** 
+ * 任务链头结点
+ */ 
 static ktask_obj_t _head = 
 {
     .name = "head",
@@ -12,6 +20,207 @@ static ktask_obj_t _head =
 };
 
 
+static void _reset_obj(ktask_obj_t *obj);
+static void _update_period_max(void);
+static int8_t _add_obj(ktask_obj_t *new_obj);
+static int8_t _delete_obj(ktask_obj_t *tar_obj);
+
+
+/** 
+ * @brief      创建任务
+ * @param[in]  obj 任务控制块
+ * @param[in]  name 任务名字 长度不能超过KTASK_NAME_MAX个字节
+ * @param[in]  new_task 任务函数
+ * @param[in]  period_ms 任务执行周期 单位为毫秒
+ * @param[in]  priority 任务优先级 同执行周期下 越大越先执行
+ * @param[in]  arg 参数
+ * @retval     
+ * @note       
+ */ 
+ktask_obj_t * ktask_create(ktask_obj_t *obj, const char *name, ktask_ptr new_task, uint32_t period_ms, uint8_t priority, void *arg)
+{
+    if((obj == NULL) || (name == NULL) || (new_task == NULL))
+        return NULL; 
+
+    _reset_obj(obj);
+
+    _K_MEMCPY(obj->name, name, KTASK_NAME_MAX);
+    obj->period_ms = period_ms;
+    obj->flag |= KTASK_FLAG_OPEN;
+    obj->priority = priority;
+    obj->task = new_task;
+    obj->arg = arg;
+    
+    return obj;
+}
+
+
+/** 
+ * @brief      开始任务
+ * @param[in]  obj 任务控制块
+ * @retval     0-成功 -1-参数错误
+ * @note       
+ */ 
+int8_t ktask_start(ktask_obj_t *obj)
+{
+    return _add_obj(obj);
+}
+
+
+/** 
+ * @brief      删除任务
+ * @param[in]  obj 任务控制块
+ * @retval     0-成功 -1-参数错误 -2-任务未初始化
+ * @note       
+ */ 
+int8_t ktask_delete(ktask_obj_t *obj)
+{
+    if(obj == NULL)
+        return -1; 
+        
+    if(!(obj->flag & KTASK_FLAG_OPEN))
+        return -2;
+
+    obj->flag = KTASK_FLAG_CLOSE;
+
+    return _delete_obj(obj);
+}
+
+
+/** 
+ * @brief      挂起任务
+ * @param[in]  obj 任务控制块
+ * @retval     0-成功 -1-参数错误 -2-任务未初始化
+ * @note       
+ */ 
+int8_t ktask_suspend(ktask_obj_t *obj)
+{
+    if(obj == NULL)
+        return -1; 
+        
+    if(!(obj->flag & KTASK_FLAG_OPEN))
+        return -2;
+
+    obj->flag |= KTASK_FLAG_SUSPEND;
+
+    _update_period_max();
+
+    return 0;
+}
+
+
+/** 
+ * @brief      恢复任务
+ * @param[in]  obj 任务控制块
+ * @retval     0-成功 -1-参数错误 -2-任务未初始化
+ * @note       
+ */ 
+int8_t ktask_resume(ktask_obj_t *obj)
+{
+    if(obj == NULL)
+        return -1; 
+        
+    if(!(obj->flag & KTASK_FLAG_OPEN))
+        return -2;
+
+    obj->flag &= (~KTASK_FLAG_SUSPEND);
+
+    return 0;
+}
+
+
+/** 
+ * @brief      打印当前任务
+ * @param      无
+ * @retval     无
+ * @note       
+ */ 
+void ktask_list(void)
+{
+    ktask_obj_t *_obj = _head.next;
+
+    while(_obj)
+    {
+        _K_PRINTF("Name: %s, Period: %d, Priority: %d, State: 0x%02X", _obj->name, \
+                    _obj->period_ms, _obj->priority, _obj->flag);
+
+        _obj = _obj->next;
+    }
+}
+
+
+/** 
+ * @brief      任务调度函数
+ * @param      无
+ * @retval     无
+ * @note       
+ */ 
+void ktask_schedule(void)
+{
+    ktask_obj_t *_obj = _head.next;
+
+    while(_obj)
+    {
+        if(_obj->flag & _KTASK_FLAG_RUN)
+        {
+            _obj->task(_obj->arg);
+            _obj->flag &= (~_KTASK_FLAG_RUN);
+        }
+
+        _obj = _obj->next;
+    }
+}
+
+
+/** 
+ * @brief      任务滴答函数
+ * @param      无
+ * @retval     无
+ * @note       
+ */ 
+void ktask_tick(uint16_t period)
+{
+    ktask_obj_t *_obj = _head.next;
+    uint16_t _tick_max = _period_max / period;
+    _tick++;
+
+    while(_obj)
+    {
+        _obj->tick = _obj->period_ms / period;
+        
+        if(_tick_max < _obj->tick)
+            _tick_max = _obj->tick;
+
+        if(((_tick % _obj->tick) == 0) && (!(_obj->flag & KTASK_FLAG_SUSPEND)))
+            _obj->flag |= _KTASK_FLAG_RUN;
+
+        _obj = _obj->next;
+    }
+
+    if(_tick >= _tick_max)
+        _tick = 0;
+}
+
+
+/** 
+ * @brief      任务错误
+ * @param      无
+ * @retval     无
+ * @note       
+ */ 
+void ktask_assert(void)
+{
+    _K_PRINTF("assert!!!");
+    while(1);
+}
+
+
+/** 
+ * @brief      任务控制块初始化
+ * @param[in]  obj 任务控制块
+ * @retval     无
+ * @note       
+ */ 
 static void _reset_obj(ktask_obj_t *obj)
 {
     _K_MEMSET(obj->name, '\0', KTASK_NAME_MAX);
@@ -22,6 +231,12 @@ static void _reset_obj(ktask_obj_t *obj)
 }
 
 
+/** 
+ * @brief      更新当前任务链中最大的周期
+ * @param      无
+ * @retval     无
+ * @note       
+ */ 
 static void _update_period_max(void)
 {
     ktask_obj_t *_obj = _head.next;
@@ -39,6 +254,12 @@ static void _update_period_max(void)
 }
 
 
+/** 
+ * @brief      插入任务块
+ * @param[in]  new_obj 待插入的任务控制块
+ * @retval     0-成功 -1-参数错误
+ * @note       
+ */ 
 static int8_t _add_obj(ktask_obj_t *new_obj)
 {
     ktask_obj_t *_obj = _head.next;
@@ -69,6 +290,13 @@ static int8_t _add_obj(ktask_obj_t *new_obj)
     return 0;
 }
 
+
+/** 
+ * @brief      删除任务块
+ * @param[in]  tar_obj 待删除的任务控制块
+ * @retval     0-成功 -1-参数错误
+ * @note       
+ */ 
 static int8_t _delete_obj(ktask_obj_t *tar_obj)
 {
     ktask_obj_t *_obj = _head.next;
@@ -92,186 +320,5 @@ static int8_t _delete_obj(ktask_obj_t *tar_obj)
 
     return 0;
 }
-
-
-int8_t ktask_create(ktask_obj_t *obj, const char *name, ktask_ptr new_task, uint32_t period_ms, uint8_t priority, void *arg)
-{
-    if((obj == NULL) || (name == NULL) || (new_task == NULL))
-        return -2; 
-
-    _reset_obj(obj);
-
-    _K_MEMCPY(obj->name, name, KTASK_NAME_MAX);
-    obj->period_ms = period_ms;
-    obj->flag |= KTASK_FLAG_OPEN;
-    obj->priority = priority;
-    obj->task = new_task;
-    obj->arg = arg;
-
-    return _add_obj(obj);
-}
-
-int8_t ktask_delete(ktask_obj_t *obj)
-{
-    if(obj == NULL)
-        return -1; 
-        
-    if(!(obj->flag & KTASK_FLAG_OPEN))
-        return -2;
-
-    obj->flag = KTASK_FLAG_CLOSE;
-
-    return _delete_obj(obj);
-}
-
-
-int8_t ktask_suspend(ktask_obj_t *obj)
-{
-    if(obj == NULL)
-        return -1; 
-        
-    if(!(obj->flag & KTASK_FLAG_OPEN))
-        return -2;
-
-    obj->flag |= KTASK_FLAG_SUSPEND;
-
-    _update_period_max();
-
-    return 0;
-}
-
-int8_t ktask_resume(ktask_obj_t *obj)
-{
-    if(obj == NULL)
-        return -1; 
-        
-    if(!(obj->flag & KTASK_FLAG_OPEN))
-        return -2;
-
-    obj->flag &= (~KTASK_FLAG_SUSPEND);
-
-    return 0;
-}
-
-
-void ktask_list(void)
-{
-    ktask_obj_t *_obj = _head.next;
-
-    while(_obj)
-    {
-        _K_PRINTF("name: %s, pri: %d", _obj->name, _obj->priority);
-
-        _obj = _obj->next;
-    }
-}
-
-
-void ktask_schedule(void)
-{
-    ktask_obj_t *_obj = _head.next;
-
-    while(_obj)
-    {
-        if(_obj->flag & _KTASK_FLAG_RUN)
-        {
-            _obj->task(_obj->arg);
-            _obj->flag &= (~_KTASK_FLAG_RUN);
-        }
-
-        _obj = _obj->next;
-    }
-}
-
-void ktask_tick(uint16_t period)
-{
-    ktask_obj_t *_obj = _head.next;
-    uint16_t _tick_max = _period_max / period;
-    _tick++;
-
-    while(_obj)
-    {
-        _obj->tick = _obj->period_ms / period;
-        
-        if(_tick_max < _obj->tick)
-            _tick_max = _obj->tick;
-
-        if(((_tick % _obj->tick) == 0) && (!(_obj->flag & KTASK_FLAG_SUSPEND)))
-            _obj->flag |= _KTASK_FLAG_RUN;
-
-        _obj = _obj->next;
-    }
-
-    if(_tick >= _tick_max)
-        _tick = 0;
-}
-
-
-void ktask_assert(void)
-{
-    _K_PRINTF("assert!!!");
-    while(1);
-}
-
-
-
-
-
-
-#include "unistd.h"
-
-int time = 0;
-void test(void *arg)
-{
-    // if(*(char *)arg == '3')
-    // {
-    //     time = 2;
-    //     printf("change time~~~\r\n");
-    // }
-
-    printf("TICK: %d | task %s\r\n", _tick, (char *)arg);
-}
-
-int main(int argc, char const *argv[])
-{
-    ktask_obj_t obj[5];
-    static char str_task[][KTASK_NAME_MAX] = {
-        "1",
-        "2",
-        "3",
-        "4",
-        "5",
-    };
-
-    ktask_create(&obj[0], "task1", test, 10, 3, str_task[0]);
-    ktask_create(&obj[1], "task2", test, 2, 6, str_task[1]);
-    ktask_create(&obj[2], "task3", test, 4, 2, str_task[2]);
-    ktask_create(&obj[3], "task4", test, 2, 5, str_task[3]);
-    ktask_create(&obj[4], "task5", test, 8, 9, str_task[4]);
-
-    ktask_list();
-
-    ktask_delete(&obj[0]);
-
-    ktask_suspend(&obj[4]);
-
-    ktask_list();
-
-    time = 1;
-
-    while(1)
-    {
-        ktask_tick(time);
-        ktask_schedule();
-        sleep(time);
-    }
-
-    return 0;
-}
-
-
-
-
-
 
 
--- k_rtos.h
+++ k_rtos.h
@@ -4,6 +4,8 @@
 #include "stdint.h"
 #include "string.h"
 #include "stdio.h"
+#include "rtthread.h"
+
 
 #define KTASK_NAME_MAX          8
 #define KTASK_TICK_INTERVAL     1           //1ms
@@ -16,9 +18,7 @@
 
 #define _K_MEMCPY            memcpy
 #define _K_MEMSET            memset 
-#define _K_PRINTF(fmt,...)   printf("[ktask]: "fmt"\r\n",##__VA_ARGS__)
-
-
+#define _K_PRINTF(fmt,...)   rt_kprintf("[ktask]: "fmt"\r\n",##__VA_ARGS__)
 
 
 typedef void (*ktask_ptr)(void *arg);
@@ -33,18 +33,95 @@ struct ktask_obj
     uint8_t flag;
     uint8_t priority;
     ktask_ptr task;
-
     void *arg;
-
     ktask_obj_t *next;
 };
 
 
+/** 
+ * @brief      创建任务
+ * @param[in]  obj 任务控制块
+ * @param[in]  name 任务名字 长度不能超过KTASK_NAME_MAX个字节
+ * @param[in]  new_task 任务函数
+ * @param[in]  period_ms 任务执行周期 单位为毫秒
+ * @param[in]  priority 任务优先级 同执行周期下 越大越先执行
+ * @param[in]  arg 参数
+ * @retval     
+ * @note       
+ */ 
+ktask_obj_t *ktask_create(ktask_obj_t *obj, const char *name, ktask_ptr new_task, uint32_t period_ms, uint8_t priority, void *arg);
 
 
+/** 
+ * @brief      开始任务
+ * @param[in]  obj 任务控制块
+ * @retval     0-成功 -1-参数错误
+ * @note       
+ */ 
+int8_t ktask_start(ktask_obj_t *obj);
 
 
+/** 
+ * @brief      删除任务
+ * @param[in]  obj 任务控制块
+ * @retval     0-成功 -1-参数错误 -2-任务未初始化
+ * @note       
+ */ 
+int8_t ktask_delete(ktask_obj_t *obj);
 
+
+/** 
+ * @brief      挂起任务
+ * @param[in]  obj 任务控制块
+ * @retval     0-成功 -1-参数错误 -2-任务未初始化
+ * @note       
+ */ 
+int8_t ktask_suspend(ktask_obj_t *obj);
+
+
+/** 
+ * @brief      恢复任务
+ * @param[in]  obj 任务控制块
+ * @retval     0-成功 -1-参数错误 -2-任务未初始化
+ * @note       
+ */ 
+int8_t ktask_resume(ktask_obj_t *obj);
+
+
+/** 
+ * @brief      打印当前任务
+ * @param      无
+ * @retval     无
+ * @note       
+ */ 
+void ktask_list(void);
+
+
+/** 
+ * @brief      任务调度函数
+ * @param      无
+ * @retval     无
+ * @note       
+ */ 
+void ktask_schedule(void);
+
+
+/** 
+ * @brief      任务滴答函数
+ * @param      无
+ * @retval     无
+ * @note       
+ */ 
+void ktask_tick(uint16_t period);
+
+
+/** 
+ * @brief      任务错误
+ * @param      无
+ * @retval     无
+ * @note       
+ */ 
+void ktask_assert(void);
 
 
 
